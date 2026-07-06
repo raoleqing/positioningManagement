bbb73d9ef96a1c787fea16fd5a1b34720ff975f0
SY100836 <SY100836@hnlens.com>
2026-07-06 15:05:04 +0800

    创建项目

---
+++ src/main/java/com/zns/positioning/positioningmanagement/PositioningManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zns.positioning.positioningmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositioningManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositioningManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zns/positioning/positioningmanagement/PositioningManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zns.positioning.positioningmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositioningManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
